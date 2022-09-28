c15755e14449d8cdd43a6e2bb9188f2afb849cb1
jorgemsfabc <104578253+jorgemsfabc@users.noreply.github.com>
2022-09-28 09:30:58 -0300

    1.	Implemente as TADs número Racional e número Complexo

--- main.c
+++ main.c
@@ -1,20 +1,20 @@
 #include "racional.c"
 
-int main() {
+void main() {
 
    RACIONAL *r1=criar(1,2);
    RACIONAL *r2=criar(1,2);
 
-   imprimir(r1);
-   imprimir(r2);
-
    RACIONAL *r3 = adicao(r1,r2);
+   printf("Adição: ");
    imprimir(r3);
 
    RACIONAL *r4 = multiplicacao(r1,r2);
+   printf("Multiplicação: ");
    imprimir(r4);
 
    RACIONAL *r5 = divisao(r1,r2);
+   printf("Divisão: ");
    imprimir(r5);
    
    limpar(r1);
@@ -22,7 +22,6 @@ int main() {
    limpar(r3);
    limpar(r4);
    limpar(r5);
-
-   return 0;
+  
 }
 
